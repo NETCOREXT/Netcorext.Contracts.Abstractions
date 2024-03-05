e0ee460aa7bd96a12290423465ce9a28492a6f19
Aren Chen <gucci313031@gmail.com>
2024-03-05 13:55:59 +0800

    add RequestId

--- IContextState.cs
+++ IContextState.cs
@@ -4,6 +4,7 @@ namespace Netcorext.Contracts;
 
 public interface IContextState
 {
+    string? RequestId { get; set; }
     ClaimsPrincipal? User { get; set; }
     IDictionary<object, object?> Items { get; set; }
 }
